93ba67d18993ef13af9ed5bfdae823a024594b83
Mr Third <vuanlbv@gmail.com>
2019-03-11 11:23:00 +0700

    update claimrequest dto

--- Model/ClaimRequest.cs
+++ Model/ClaimRequest.cs
@@ -32,7 +32,7 @@ namespace DTO
         public int ProjectId { get; set; }
 
         [Display(Name = "Approver Id")]
-        [ForeignKey("Staff")]
+        [ForeignKey("Approver")]
         [Required(ErrorMessage = "Please enter Approver Id")]
         public int ApproverId { get; set; }
 
@@ -49,6 +49,7 @@ namespace DTO
         public string AuditTrail { get; set; }
 
         public virtual Staff Staff { get; set; }
+        public virtual Staff Approver { get; set; }
         public virtual Project Project { get; set; }
         public virtual ICollection<ClaimRequestDetail> ClaimRequestDetails { get; set; }
     }
